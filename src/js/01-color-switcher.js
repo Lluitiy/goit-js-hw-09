1f99068073871154375961749d46fec0fce9608e
Pavlo Vovzhynskyy <pavlovovzhynskyy@gmail.com>
2022-08-25 03:45:47 +0200

    9/ 1st and 2nd done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+btnStart.style.backgroundColor = '#198754';
+btnStart.style.color = '#fff';
+btnStart.style.borderRadius = '15px';
+btnStart.style.borderColor = 'green';
+
+
+btnStop.style.backgroundColor = '#dc3545';
+btnStop.style.color = '#fff';
+btnStop.style.borderRadius = '15px';
+btnStop.style.borderColor = 'red';
+
+
+const INTERVAL = 1000;
+let intervalId = null;
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', bgColorChange);
+
+function bgColorChange() {
+	btnStart.disabled = true;
+
+	intervalId = setInterval(() => {
+		let bgColor = `${getRandomHexColor()}`;
+		body.style.backgroundColor = bgColor;
+	}, INTERVAL);
+
+	btnStop.addEventListener('click', bgColorStopChange);
+
+	function bgColorStopChange() {
+		btnStart.disabled = false;
+		clearInterval(intervalId);
+	}
+}
